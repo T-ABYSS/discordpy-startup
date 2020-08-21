091a90c7f71ac605bf5854937f417890f018f220
T-ABYSS <70027194+T-ABYSS@users.noreply.github.com>
2020-08-22 04:45:16 +0900

    modify

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,7 @@
-import boto3
 from discord.ext import commands
 import os
 import traceback
+import urllib.request
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -15,9 +15,9 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def startserver(ctx):
-#    client = boto3.client('ec2')
-#    response = ec2_client().start_instances(instances=EC2_INSTANCE_ID)
-    await ctx.send('Accept.')
+    req = urllib.request.Request('API_URL')
+    with urllib.request.urlopen(req) as res:
+    await ctx.send(res.read())
 
 
 bot.run(token)
