12906e8177d718be201575da5bea9a22b4a19d2c
T-ABYSS <70027194+T-ABYSS@users.noreply.github.com>
2020-08-22 03:48:23 +0900

    modify

--- discordbot.py
+++ discordbot.py
@@ -8,16 +8,6 @@ InstanceId = 'EC2_INCETANCE_ID'
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-
-def start_instances():
-    client = boto3.client('ec2')
-    response = ec2_client().start_instances(
-        InstanceIds=[
-            InstanceId
-        ]
-    )
-
-
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
@@ -27,8 +17,13 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def stert_server(ctx):
-    await start_instances()
-    await ctx.send('起動命令を受け付けました')
+    client = boto3.client('ec2')
+    response = ec2_client().start_instances(
+        InstanceIds=[
+            InstanceId
+        ]
+    )
+    await ctx.send('Accept.')
 
 
 bot.run(token)
