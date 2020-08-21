4f94784b55ea678fa57c70dad38d577dbdc8b163
T-ABYSS <70027194+T-ABYSS@users.noreply.github.com>
2020-08-22 04:00:01 +0900

    test

--- discordbot.py
+++ discordbot.py
@@ -1,11 +1,11 @@
 from discord.ext import commands
 import os
 import traceback
-import boto3
+#import boto3
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-instance = 'EC2_INCETANCE_ID'
+#instance = 'EC2_INCETANCE_ID'
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -15,7 +15,7 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def stert_server(ctx):
+async def start_server(ctx):
 #    client = boto3.client('ec2')
 #    response = ec2_client().start_instances(instances=instance)
     await ctx.send('Accept.')
